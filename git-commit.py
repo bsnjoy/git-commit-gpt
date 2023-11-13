118094da63749d019e5bcd5a11c2074c82010677
s-Macbook-Air-m1 <bsnjoy@gmail.com>
2023-11-13 18:26:09 +0700

    "Updated the code to use OpenAI's GPT-4 model for generating commit messages. Replaced the requests library with the OpenAI library for API calls. Also, improved error handling for unsuccessful API responses and JSON decoding errors."

--- git-commit.py
+++ git-commit.py
@@ -1,45 +1,45 @@
 #!/usr/bin/env python3
 import subprocess
-import requests
 import json
 import config
+import openai
+
+openai.api_key = config.OPENAI_API_KEY
 
 def get_git_diff():
     result = subprocess.run(["git", "diff"], stdout=subprocess.PIPE)
     return result.stdout.decode()
 
 def generate_commit_message(diff):
-    prompt = f"Generate a Git commit message based on these changes:\n\n{diff}"
+    prompt = f'''Write a comment, which I will put in git commit command for changes below git diff.
+ Include description of what was changed so that user, who is not familiar with the programming will understand what has been updated. 
+ Don't write which files were changed. 
+ 1. output with no introduction, no explaintation, only comment.
+ 2. DONT MAKE ANY MISTAKES, check if you did any
+ 3. only return comment, and nothing else.
+ 4. no DOCUMENATION IN THE OUTPUT
 
-    headers = {
-        'Authorization': f'Bearer {config.OPENAI_API_KEY}',
-        'Content-Type': 'application/json'
-    }
+git diff:\n{diff}'''
 
-    data = {
-        'model': 'gpt-4.0-turbo',
-        'prompt': prompt,
-        'max_tokens': 150
-    }
-
-    response = requests.post('https://api.openai.com/v1/engines/gpt-4.0-turbo/completions', headers=headers, json=data)
+    completion = openai.ChatCompletion.create( # Change the function Completion to ChatCompletion
+  model = config.OPENAI_API_MODEL,
+  messages = [ # Change the prompt parameter to the messages parameter
+    {"role": "system", "content": "You are a helpful assistant."},
+    {'role': 'user', 'content': prompt}
+  ],
+  temperature = 0  
+)
     
-    # Check if the response is successful
-    if response.status_code != 200:
-        print(f"Error: Received status code {response.status_code}")
-        print("Response content:", response.text)
-        return False, "Error in generating commit message"
-
     try:
-        return True, response.json()['choices'][0]['text'].strip()
+        return True, completion.choices[0].message.content
     except KeyError:
         print("Error: 'choices' key not found in response.")
-        print("Response content:", response.text)
+        print("Response content:", completion.text)
         return False, "Error in generating commit message"
 
     except json.JSONDecodeError:
         print("Error: Unable to decode JSON response.")
-        print("Response content:", response.text)
+        print("Response content:", completion.text)
         return False, "Error in generating commit message"
 
 def main():
